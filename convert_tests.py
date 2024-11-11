c0fbe262a82d66139ef9d26e6b60234a79bee296
groom <cgroom@calpoly.edu>
2024-11-10 19:10:54 -0600

    Final Code

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,20 @@
+import unittest
 
+from convert import str_to_float
+
+
+class TestCases(unittest.TestCase):
+    pass
+
+    # Part 1
+    def test_str_to_float1(self):
+        self.assertEqual(str_to_float("5"), 5.0)
+
+    def test_str_to_float2(self):
+        self.assertEqual(str_to_float("xyz"), None)
+
+    # Part 2
+
+
+if __name__ == '__main__':
+    unittest.main()
